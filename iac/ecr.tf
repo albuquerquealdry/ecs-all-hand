d6301cb0d5952c8ea9d88e9f07027921ab997cc9
Aldry Albuquerque <albuquerquealdry>
2023-08-16 18:24:23 -0400

    Adicionar ECR Module

---
+++ iac/ecr.tf
@@ -0,0 +1,27 @@
+module "ecr" {
+  source = "terraform-aws-modules/ecr/aws"
+
+  repository_name = "blog-do-raffa-moreira"
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 3
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
